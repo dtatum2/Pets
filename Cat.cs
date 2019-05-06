dee3ac234c0fd8bf699b29640dd8b46c11c4f42b
Damon Tatum <damontatumii@mwc-021104.dhcp.missouri.edu>
2019-05-06 14:41:40 -0500

    first commit

---
+++ Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Pets
+{
+    public class Cat : Pet
+    {
+        public Cat(string name, string owner, double weight) : base("cat", name, owner, weight)
+        {
+        }
+
+        public string meow(int count)
+        {
+            string meows = "";
+            for (int i = 0; i < count; i++)
+            {
+                meows += "meow.";
+            }
+
+            return meows;
+        }
+    }
+}
---
+++ Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Pets
+{
+    public class Dog : Pet
+    {
+        public Dog(string name, string owner, double weight) : base("dog", name, owner, weight)
+        {
+        }
+
+        public string bark(int count)
+        {
+            string barks = "";
+            for (int i = 0; i < count; i++)
+            {
+                barks += "bark!";
+            }
+
+            return barks;
+        }
+    }
+}
